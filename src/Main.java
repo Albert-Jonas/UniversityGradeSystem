ab373a34f6ab721ca95f99568004057339e1ccb7
Albert <jonas.albert.oktatas@gmail.com>
2022-10-03 19:51:50 +0200

    Kiiratás stringek frissítése és calculate függvény, result field kidobása

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         read_in();
-        print_result();
+        calculate();
     }
 
 //beolvasás
@@ -27,36 +27,30 @@ public class Main {
     static int laboratory_score;
     static int written_part_score;
 
-    int result;
-
 //számítás
 
-    String calculate()
+    static void calculate()
     {
+        int result = black_board_score + laboratory_score + written_part_score;
+        System.out.println("Pontszámaid összesen : " + result);
+
         if (black_board_score < 25 || laboratory_score <25 || written_part_score < 25)
         {
-            return "bukott";
+            System.out.println("Megbuktál mert egy tárgyból nem érted el a 25 pontot, hiába van  " + result+ "  pontod");
         } else if ( black_board_score + laboratory_score + written_part_score < 76)
         {
-            return "bukott";
+            System.out.println("Megbukott, mert az összpontszám nem érte el a 76-ot  " + result);
         } else if ( black_board_score + laboratory_score + written_part_score > 75 && black_board_score + laboratory_score + written_part_score < 100)
         {
-            return "elégséges";
+            System.out.println("elégséges  " + result);
         } else if ( black_board_score + laboratory_score + written_part_score > 99 && black_board_score + laboratory_score + written_part_score < 125)
         {
-            return "jó";
+            System.out.println("jó " + result);
         } else
         {
-            return "kiváló";
+            System.out.println("kiváló " + result);
         }
 
-    }
-
-//eredmény kiírása
-
-    static void print_result()
-    {
-        //calculate() függvény meghívásával
     }
 
 }
