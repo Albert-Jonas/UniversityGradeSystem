dcca7d7e24c09a329fe2b266ad718388da4e06bc
Albert <jonas.albert.oktatas@gmail.com>
2022-10-03 19:57:46 +0200

    Teszt file hozzáadása

---
+++ src/MainTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void read_in() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculate() {
+    }
+}
