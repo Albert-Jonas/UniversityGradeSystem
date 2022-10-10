0e54962ed6f65cc57a734218c3edea68c6df3d55
Albert <jonas.albert.oktatas@gmail.com>
2022-10-10 19:59:46 +0200

    Input helyettesítés

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.BeforeEach;
-
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,14 +10,24 @@ class MainTest {
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
+    private final InputStream systemIn = System.in;
+
+    private ByteArrayInputStream testIn;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
         System.setOut(new PrintStream(outputStreamCaptor));
+
+    }
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
         System.setOut(standardOut);
+        System.setIn(systemIn);
     }
 
     @org.junit.jupiter.api.Test
@@ -26,7 +36,12 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     void read_in() {
+        final String testString = "25\n25\n25\n";
+        provideInput(testString);
 
+        Main.read_in();
+
+        assertEquals(25,Main.written_part_score);
     }
 
     @org.junit.jupiter.api.Test
